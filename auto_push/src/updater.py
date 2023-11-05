398f21de3cb27509cd8723d73ebb209eca61c073
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-05 19:25:00 +0700

    doc: add docstring

--- auto_push/src/updater.py
+++ auto_push/src/updater.py
@@ -3,7 +3,7 @@ from auto_push.src.classes.github import Github
 from auto_push.src.classes.weather import Weather
 from auto_push.src.classes.env_loader import EnvLoader
 
-# -- Load .env filec
+# -- Load .env file
 env_loader = EnvLoader()
 env_loader.load_env()
 
@@ -15,17 +15,34 @@ github = Github()
 
 
 def updater():
-    print('Updater as been call.')
+    """
+    Updates the GitHub bio with the current weather information and sends a notification.
 
-    # -- Make a request to get the current weather in bangkok.
+    The function fetches the current weather, formats it, and then updates the GitHub bio
+    with this formatted weather information. After updating GitHub, it also sets the GitHub
+    status to a specific message and sends a desktop notification to inform the user of
+    successful completion.
+
+    The weather information is fetched for Bangkok, and the GitHub objects are initialized
+    with environmental variables loaded at the beginning of the script.
+    """
+    print('Updater has been called.')
+
+    # -- Make a request to get the current weather in Bangkok.
     response = weather.get_weather()
     github_bio = weather.format_weather(response)
 
-    # -- Make a request to GitHub
+    # -- Update GitHub bio with the weather information
     github.update_bio(github_bio)
+
+    # -- Update GitHub status
     github.update_status('Working on Github API.')
+
+    # -- Send a notification to the user
     notification.notify(
-        'Auto push', 'Your Github bio has been updated successfully.')
+        title='Auto push',
+        message='Your Github bio has been updated successfully.'
+    )
 
 
 if __name__ == '__main__':
