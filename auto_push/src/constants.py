b5b0be2319856136023942dbe4c5c8ac76761b59
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-16 01:55:56 +0700

    feat: add storage opts

--- auto_push/src/constants.py
+++ auto_push/src/constants.py
@@ -6,3 +6,10 @@ __version__ = "0.1.0"
 STORAGE_LOCATION_UNIX = f"{Path(__file__).parent.parent.parent}/var"
 STORAGE_LOCATION_WINDOWS = ""
 STORAGE_LOCATION_LINUX = ""
+STORAGE_BASIC_OPTS = {
+    "app_init": True,
+    "weather_api": False,
+    "github_status": False,
+    "github_bio_custom_content": False,
+    "keyboard_handler": False
+}
