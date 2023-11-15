14b87ded2b94ec1e92491868398332d4fd7b0b09
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-16 01:55:26 +0700

    fix: set cron tab opt to false when delete jobs

--- auto_push/commands/cmd_stop.py
+++ auto_push/commands/cmd_stop.py
@@ -3,9 +3,12 @@ import typer
 from crontab import CronTab
 from rich import print
 
+from auto_push.src.classes.storage_manager import StorageManager
+
 
 # -- Variables
 app = typer.Typer(rich_help_panel="rich")
+storage_manager = StorageManager()
 current_user = os.getlogin()
 scheduler = CronTab(user=current_user)
 
@@ -17,4 +20,5 @@ def stop():
     """
     scheduler.remove_all()
     scheduler.write()
+    storage_manager.set_data("github_cron_tab", False)
     print('[bold green]All cron jobs have been removed successfully.[/bold green]')
