22f85a1989b16e8b6c8c23c16a66f2a0e4855324
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-16 15:23:51 +0700

    chore: print key, value from config file

--- auto_push/commands/cmd_checkup.py
+++ auto_push/commands/cmd_checkup.py
@@ -1,12 +1,16 @@
+import json
 import os
 import typer
 from pathlib import Path
 from rich import print
 from rich.table import Table
 
+from auto_push.src.classes.storage_manager import StorageManager
+
 
 # -- Variables
 app = typer.Typer(rich_help_panel="rich")
+storage_manager = StorageManager()
 
 
 @app.command(rich_help_panel="Utils & Configs")
@@ -15,7 +19,11 @@ def checkup():
     Prints the current environment variable values set in the .env file.
     """
     try:
+        # -- Variables
         env_path = Path(__file__).parent.parent.parent / '.env'
+        location = storage_manager.get_data("location")
+        full_file_path = os.path.join(location, "auto_push.json")
+
         if env_path.exists():
             print(
                 f"[bold underline blue]Current .env file contents:\n[/bold underline blue]")
@@ -29,6 +37,19 @@ def checkup():
             print(table)
         else:
             print("[bold yellow italic]No .env file found.[/bold yellow italic]")
+
+        with open(full_file_path, "r+") as file:
+            data = json.load(file)
+            table = Table("Key", "Value")
+            for key, value in data.items():
+                table.add_row(
+                    f"[yellow]{key}[/yellow]",
+                    "[purple]Activated[/purple]" if value is True else "[purple]Deactivated[/purple]" if value is False else f"[purple]{value}[/purple]"
+                )
+            print(
+                f"[bold underline blue]Current configuration file contents:\n[/bold underline blue]")
+            print(table)
+
     except Exception as e:
         print(
-            f"[bold red]An error occurred while reading the .env file: {e}[/bold red]")
+            f"[bold red]An error occurred while reading configuration: {e}[/bold red]")
