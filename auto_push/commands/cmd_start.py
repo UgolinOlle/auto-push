9d7abead503754b4d794a6f122b68902bce989e7
Ugolin Ollé <hello@ugolin-olle.com>
2023-11-16 15:24:38 +0700

    feat: add custom content to github bio

--- auto_push/commands/cmd_start.py
+++ auto_push/commands/cmd_start.py
@@ -5,24 +5,75 @@ from crontab import CronTab
 from pathlib import Path
 from rich import print
 
+from auto_push.src.classes.storage_manager import StorageManager
 
 # -- Variables
 app = typer.Typer(rich_help_panel="rich")
+storage_manager = StorageManager()
 current_user = os.getlogin()
 scheduler = CronTab(user=current_user)
 
 
-@app.command(rich_help_panel="Commands")
-def start():
+def github_update(minute: int = 0, hour: int = 0, day: int = 0, month: int = 0):
     """
-    Create cron job to run the updater script every 6 hours.
+    Set up a cron job with the specified scheduling.
+    """
+    script_path = Path.cwd() / "auto_push" / "src" / "updater.py"
+    job = scheduler.new(command=f"{sys.executable} {script_path}", comment="1")
+    job.minute.on(minute if 0 <= minute <= 59 else 0)
+    job.hour.on(hour if hour > 0 else 1)
+    job.day.on(day if 0 > day <= 31 else 1)
+    job.month.on(month if 0 > month <= 31 else 1)
+    scheduler.write()
+
+
+@app.command()
+def start(content: str = typer.Option("", help="Content to set in your Github bio"),
+          minute: int = typer.Option(0, help="Minute for scheduling"),
+          hour: int = typer.Option(6, help="Hour for scheduling"),
+          day: int = typer.Option(0, help="Day for scheduling"),
+          month: int = typer.Option(0, help="Month for scheduling")):
+    """
+    Create or reset a cron job to run the updater script.
+
+    Parameters:
+    -----------
+    content: The content of your Github bio.
+    minute: The minute when the cron job should run. Defaults to 0.
+    hour: The hour when the cron job should run. Defaults to 6.
+    day: The day of the month when the cron job should run. Defaults to 0 (every day).
+    month: The month when the cron job should run. Defaults to 0 (every month).
     """
     try:
-        script_path = Path.cwd() / "auto_push" / "src" / "updater.py"
-        job = scheduler.new(command=f"{sys.executable} {script_path}")
-        job.hour.every(6)
-        scheduler.write()
-        print('[bold green]Github bio update has been set successfully.[/bold green]')
+        user_job: str = storage_manager.get_data("github_cron_tab")
+        if content != "":
+            storage_manager.set_data("github_bio_custom_content", True)
+            storage_manager.set_data("github_bio_content", content)
+            storage_manager.set_data("weather_api", False)
+        else:
+            storage_manager.set_data("github_bio_custom_content", False)
+            storage_manager.set_data("weather_api", True)
+        if user_job is not True:
+            github_update(minute=minute,
+                          hour=hour, day=day, month=month)
+            storage_manager.set_data("github_cron_tab", True)
+            print(
+                '[bold green]Github bio update has been set successfully.[/bold green]')
+        elif user_job is None:
+            print(
+                '[bold red]An error occured in your configuration file. Automatically reset cli config...[/bold red]')
+            storage_manager.fix_storage()
+            print(
+                '[bold green]CLI configuration as been reset successfully.[/bold green]'
+            )
+        else:
+            print('[bold yellow]Github bio update is already setup.[/bold yellow]')
+            reset = typer.prompt("Do you want to reset ? (yes/no)")
+            if reset.lower() == 'yes':
+                scheduler.remove_all(comment="1")
+                github_update(minute=minute,
+                              hour=hour, day=day, month=month)
+                print(
+                    '[bold green]Github bio update has been reset successfully.[/bold green]')
     except Exception as e:
-        print(
-            f"[bold red]An error occurred while setting up the cron job: {e}[/bold red]")
+        print(f"[bold red]An error occurred while setting up the cron job. Sorry for this, we already send a message to support.[/bold red]")
