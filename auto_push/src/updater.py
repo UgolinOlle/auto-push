9394426d5223ee76024ba4dd6b906c6e5337d7dc
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-06 22:57:25 +0700

    docs: ✏️ update docstring correctly

--- auto_push/src/updater.py
+++ auto_push/src/updater.py
@@ -1,4 +1,3 @@
-import os
 from plyer import notification
 from rich import print
 
@@ -6,6 +5,7 @@ from auto_push.src.classes.env_loader import EnvLoader
 from auto_push.src.classes.github import Github
 from auto_push.src.classes.storage_manager import StorageManager
 from auto_push.src.classes.weather import Weather
+from auto_push.src.decorators.error_issue import error_issue
 
 # -- Load .env file
 env_loader = EnvLoader()
@@ -17,48 +17,51 @@ github = Github()
 storage_manager = StorageManager()
 
 
+@error_issue(github)
 def updater():
     """
-    Updates the GitHub bio with the current weather information and sends a notification.
+    Updates the GitHub bio with either custom content or current weather information and sends a desktop notification.
 
-    The function fetches the current weather, formats it, and then updates the GitHub bio
-    with this formatted weather information. After updating GitHub, it also sets the GitHub
-    status to a specific message and sends a desktop notification to inform the user of
-    successful completion.
+    This function first checks the configuration to determine whether to use custom content or weather information for the GitHub bio. If using weather information, it fetches the current weather data for Bangkok, formats it, and updates the GitHub bio with this information. In case of custom content, it retrieves and sets the pre-defined content from the storage manager. The function also handles any configuration errors by setting the GitHub bio to an empty string and printing an error message.
 
-    The weather information is fetched for Bangkok, and the GitHub objects are initialized
-    with environmental variables loaded at the beginning of the script.
+    After updating the GitHub bio, the function updates the GitHub status message and sends a desktop notification to inform the user that the bio has been updated successfully.
+
+    The function is decorated with `error_issue`, which automatically creates a GitHub issue if an exception occurs during execution.
+
+    Attributes:
+    -----------
+    github_bio_custom_content (bool): Flag to determine whether to use custom content or weather information.
+    weather_api (bool): Flag to indicate if weather API should be used for fetching weather data.
+    github_bio (str): The content to be updated in the GitHub bio.
+
+    Raises:
+    -------
+    Exception: Propagates any exceptions caught during execution to the `error_issue` decorator.
     """
+    github_bio_custom_content = storage_manager.get_data(
+        "github_bio_custom_content")
+    weather_api = storage_manager.get_data("weather_api")
 
-    try:
-        github_bio_custom_content = storage_manager.get_data(
-            "github_bio_custom_content")
-        weather_api = storage_manager.get_data("weather_api")
-
-        if github_bio_custom_content is True and weather_api is False:
-            github_bio = storage_manager.get_data("github_bio_content")
-        elif github_bio_custom_content is False and weather_api is True:
-            # -- Make a request to get the current weather in Bangkok.
-            response = weather.get_weather()
-            github_bio = weather.format_weather(response)
-        else:
-            print("[bold red]There is an error in the configuration file. You cannot have custom content and weather api set in same time.[/bold red]")
-
-        # -- Update GitHub bio with the weather information
-        github.update_bio(github_bio)
-
-        # -- Update GitHub status
-        github.update_status('Working on Github API.')
-
-        # -- Send a notification to the user
-        notification.notify(
-            title='Auto push',
-            message='Your Github bio has been updated successfully.'
-        )
-    except Exception as e:
+    if github_bio_custom_content is True and weather_api is False:
+        github_bio = storage_manager.get_data("github_bio_content")
+    elif github_bio_custom_content is False and weather_api is True:
+        # -- Make a request to get the current weather in Bangkok.
+        response = weather.get_weather()
+        github_bio = weather.format_weather(response)
+    else:
+        github_bio = ""
         print(
-            f"[bold red]An error occurred during update Github bio. Support as been already informed.[/bold red]")
+            "[bold red]There is an error in the configuration file. You cannot have custom content and weather api set in same time.[/bold red]"
+        )
+
+    # -- Update GitHub status
+    github.update_bio(content=github_bio)
+
+    # -- Send a notification to the user
+    notification.notify(
+        title="Auto push", message="Your Github bio has been updated successfully."
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     updater()
