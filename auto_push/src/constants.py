e9b5e27d3975f92a6787d801742144fb47567782
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-06 23:13:35 +0700

    docs: ✏️ update docstring

--- auto_push/src/constants.py
+++ auto_push/src/constants.py
@@ -1,5 +1,34 @@
 from pathlib import Path
 
+"""
+Configuration for storage and basic options for the application.
+
+This module sets up various configurations related to storage and default options for the application. It defines paths for storage based on different operating systems and sets default values for various application settings.
+
+Attributes:
+----------
+__version__ (str): Indicates the current version of the application.
+
+STORAGE_LOCATION_UNIX (str): The file path for storage on Unix-based systems.
+    It is dynamically set relative to the location of this script file.
+
+STORAGE_LOCATION_WINDOWS (str): The file path for storage on Windows systems.
+    Currently, this is set as an empty string, indicating no default path is set.
+
+STORAGE_LOCATION_LINUX (str): The file path for storage on Linux systems.
+    Currently, this is set as an empty string, indicating no default path is set.
+
+STORAGE_BASIC_OPTS (dict): A dictionary containing basic configuration options for the application.
+    These options include flags for app initialization, weather API usage, GitHub status updates,
+    custom content for GitHub bio, and keyboard handler activation.
+
+Notes:
+-----
+- The storage locations for Windows and Linux are placeholders and should be configured as needed.
+- The '__version__' attribute should be updated with each new release of the application.
+- The 'STORAGE_BASIC_OPTS' dictionary serves as a central place to manage the application's default settings.
+"""
+
 __version__ = "0.1.0"
 
 # -- Storage
